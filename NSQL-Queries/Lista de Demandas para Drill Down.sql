8d65f341f2401a60c5ba7dc05cbd6e3df35641da
Thiago Bottoni <bottonithiago@gmail.com>
2015-07-16 11:44:07 -0300

    Create Lista de Demandas para Drill Down.sql

---
+++ NSQL-Queries/Lista de Demandas para Drill Down.sql
@@ -0,0 +1,22 @@
+SELECT	@SELECT:DIM:USER_DEF:IMPLIED:IDEA:demandas.idea_id:idea_id@,
+		@SELECT:DIM_PROP:USER_DEF:IMPLIED:IDEA:demandas.nome:nome@,
+		@SELECT:DIM_PROP:USER_DEF:IMPLIED:IDEA:demandas.tipo:tipo@
+
+FROM	(
+			SELECT      inv.code idea_id
+						, inv.name nome
+						, lkp.name tipo
+			FROM        inv_ideas idea
+			INNER JOIN  inv_investments inv
+					ON  idea.id = inv.id
+			INNER JOIN  odf_ca_inv odfi
+					ON  inv.id = odfi.id
+			INNER JOIN  cmn_lookups_v lkp
+					ON  odfi.tbg_tipo = lkp.lookup_code
+					AND lkp.language_code = @where:param:language@
+			WHERE		lkp.name = 	@WHERE:PARAM:XML:STRING:/data/tbg_tipo_p/@value@
+					
+		) demandas
+		
+WHERE		@FILTER@
+HAVING		@HAVING_FILTER@
