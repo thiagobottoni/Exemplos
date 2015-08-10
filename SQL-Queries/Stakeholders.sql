753e61e5d5a977326952b11b8fa22621bdd4a617
Thiago Bottoni <bottonithiago@gmail.com>
2015-08-10 11:56:24 -0300

    Update Stakeholders.sql

--- SQL-Queries/Stakeholders.sql
+++ SQL-Queries/Stakeholders.sql
@@ -1,16 +1,16 @@
-SELECT		INV.name "Investment Name"
-		, RES.full_name "Project Manager"
-		, RES.email "Project Manager's Email"
-		, C_RES.tbg_phone "Project Manager's Phone"
-		, business_owner.name_bo "Business Owner"
-		, business_owner.email_bo "Business Owner's Email"
-		, business_owner.phone_bo "Business Owner's Phone"
-		, pmo.name_pmo "PMO Manager"
-		, pmo.email_pmo "PMO Manager's Email"
-		, pmo.phone_pmo "PMO Manager's Phone"
-		, finance_governance.name_fg "Finance and Governance"
-		, finance_governance.email_fg "Finance and Governance Email"
-		, finance_governance.phone_fg "Finance and Governance Phone"		
+SELECT		INV.name "Investimento"
+		, RES.full_name "Gerente de Projetos"
+		, RES.email "Email do Gerente de Projetos"
+		, C_RES.tbg_phone "Telefone do Gerente de Projetos" --Atributo custom (pode ser retirado da query)
+		, business_owner.name_bo "Dono do Negócio"
+		, business_owner.email_bo "E-mail do Dono do Negócio"
+		, business_owner.phone_bo "Telefone do Dono do Negócio" --Atributo custom (pode ser retirado da query)
+		, pmo.name_pmo "Gerente do PMO"
+		, pmo.email_pmo "Email do Gerente do PMO"
+		, pmo.phone_pmo "Telefone do Gerente do PMO" --Atributo custom (pode ser retirado da query)
+		, finance_governance.name_fg "G. de Finanças e governança"
+		, finance_governance.email_fg "Email do G. de Fin. e Gov."
+		, finance_governance.phone_fg "Telefone do G. de Fin. e Gov." --Atributo custom (pode ser retirado da query)	
 FROM 		INV_INVESTMENTS INV
 INNER JOIN	INV_PROJECTS PROJ
 	ON	INV.id = PROJ.prid
@@ -25,7 +25,7 @@ INNER JOIN
 			SELECT		C_PROJ_BO.id id_bo
 					, RES_BO.full_name name_bo
 					, RES_BO.email email_bo
-					, C_RES_BO.tbg_phone phone_bo
+					, C_RES_BO.tbg_phone phone_bo --Atributo custom (pode ser retirado da query)
 			FROM		ODF_CA_PROJECT C_PROJ_BO
 			INNER JOIN	SRM_RESOURCES RES_BO
 				ON	C_PROJ_BO.obj_stakeholder1 = RES_BO.id
@@ -38,7 +38,7 @@ INNER JOIN
 			SELECT		C_PROJ_PMO.id id_pmo
 					, RES_PMO.full_name name_pmo
 					, RES_PMO.email email_pmo
-					, C_RES_PMO.tbg_phone phone_pmo
+					, C_RES_PMO.tbg_phone phone_pmo --Atributo custom (pode ser retirado da query)
 			FROM		ODF_CA_PROJECT C_PROJ_PMO
 			INNER JOIN	SRM_RESOURCES RES_PMO
 				ON	C_PROJ_PMO.obj_stakeholder2 = RES_PMO.id
@@ -51,7 +51,7 @@ INNER JOIN
 			SELECT		C_PROJ_FG.id id_fg
 					, RES_FG.full_name name_fg
 					, RES_FG.email email_fg
-					, C_RES_FG.tbg_phone phone_fg
+					, C_RES_FG.tbg_phone phone_fg --Atributo custom (pode ser retirado da query)
 			FROM		ODF_CA_PROJECT C_PROJ_FG
 			INNER JOIN	SRM_RESOURCES RES_FG
 				ON	C_PROJ_FG.obj_stakeholder3 = RES_FG.id
